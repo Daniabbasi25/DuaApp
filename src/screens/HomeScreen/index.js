7db171301f02ae6b97c82ac178f87c59fb5eec90
Danyal Muneer <abbasidani82@gmail.com>
2022-10-24 17:46:42 +0500

    adding home Screen

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -1,10 +1,35 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image, ImageBackground, TouchableOpacity } from 'react-native'
 import React from 'react'
+
+import { styles } from './styles'
+import Header from '../../components/Header';
 
 const HomeScreen = () => {
   return (
+    <View style={{backgroundColor:'#fff',flex:1}}>
+      {/* <Image source={require('../../../assets/header.png')}/> */}
+    <Header />
+    <View style={styles.buttonView}>
+      <TouchableOpacity>
+        <Text style={styles.inactiveButton}>Little Kids</Text>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Text style={styles.inactiveButton}>Older Kids</Text>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Text style={styles.activeButton}>Grown Up’s</Text>
+      </TouchableOpacity>
+    </View>
+    {/* flatlist component */}
     <View>
-      <Text>HomeScreen</Text>
+
+    <View>
+      <Image source={require('../../../assets/L1.png')}/>
+      <Text>
+      Importance of Allah 
+      </Text>
+    </View>
+    </View>
     </View>
   )
 }
--- src/screens/HomeScreen/styles.js
+++ src/screens/HomeScreen/styles.js
@@ -1,3 +1,27 @@
 import { StyleSheet,Dimensions } from 'react-native'
 const {width,height}=Dimensions.get('window')
-const styles = StyleSheet.create({})
+export const styles = StyleSheet.create({
+    buttonView:{
+        flexDirection:'row',
+        justifyContent:'space-evenly',
+        marginTop:15,
+        borderBottomWidth:1,
+        
+        borderBottomColor:'rgba(196, 196, 196, 1)',
+    },
+    inactiveButton:{
+        color:'#C4C4C4',
+        // backgroundColor:'#fff',
+        // fontSize:1
+        fontWeight:'bold',
+        paddingBottom:15,
+    },
+    activeButton:{
+        color:'#000',
+        // backgroundColor:'#fff',
+        // fontSize:1
+        fontWeight:'bold',
+        borderBottomWidth:1,
+        paddingBottom:15,
+    }
+})
