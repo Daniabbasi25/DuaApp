d3f359ee3ebd2a03aa1291c7633e889a7a6e8436
Danyal Muneer <abbasidani82@gmail.com>
2022-10-24 12:40:29 +0500

    setting up the navigation folder

---
+++ src/navigations/AppRout.js
@@ -0,0 +1,18 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import MainStack from './MainStack';
+
+
+const AppRout = () => {
+  return (
+   <NavigationContainer>
+   
+
+    <MainStack />
+
+   </NavigationContainer>
+  )
+}
+
+export default AppRout
---
+++ src/screens/SplashScreen/index.js
@@ -0,0 +1,14 @@
+import { View, Text, Image } from 'react-native'
+import React from 'react'
+import SvgComponent from "./svgComponent";
+
+const SplashScreen = () => {
+  return (
+    <View>
+        
+        <SvgComponent />
+    </View>
+  )
+}
+
+export default SplashScreen
